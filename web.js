05576dc63b60219433d6f6d6e5dd5445b7591325
Cole Sobotka <cole.sobotka@gmail.com>
2013-07-12 02:29:13 +0000

    Index file changes

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexFile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+	var content = new Buffer(fs.readFileSync(indexFile, 'utf-8'));
+	response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
